cf724bf8077a703f0d3be35ec6e6698b582ea212
nummyrice <nicholasroyrice@gmail.com>
2021-10-18 11:19:00 -0700

    csrf Protection added

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const session =  require('express-session');
+const asyncHandler = require('express-async-handler')
+const { csrfProtection } = require('./utils.js');
+
+app.use(session({
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: false,
+}));
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
