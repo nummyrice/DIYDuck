2993a73f3ee04b7ada9dc0e538292f398d0a352c
suchitra <suchimohan91@gmail.com>
2021-10-19 12:42:48 -0700

    database

---
+++ db/migrations/20211019181447-create-question.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Questions');
+  }
+};
---
+++ db/models/question.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Question = sequelize.define('Question', {
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT,
+    userId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER
+  }, {});
+  Question.associate = function(models) {
+    // associations can be defined here
+  };
+  return Question;
+};
