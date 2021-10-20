5f01c5b37b7dacdd830c2b6f7d9c597ae0e46ea4
suchitra <suchimohan91@gmail.com>
2021-10-19 23:21:09 -0700

    modal done

--- public/javascripts/questionModal.js
+++ public/javascripts/questionModal.js
@@ -1,4 +1,12 @@
-const button = document.getElementById('queMdl')
+const button = document.getElementById('questionButton')
 button.addEventListener('click', async (e) => {
-    
+    const modal = document.getElementById('questionModal')
+    modal.style.display='block';
+})
+
+
+const cancelButton = document.getElementById('cancelButton')
+cancelButton.addEventListener('click', async(e)=>{
+    const modal = document.getElementById('questionModal')
+    modal.style.display='none';
 })
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const { csrfProtection, asyncHandler } = require('./utils.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'a/A Express Skeleton Home' });
+router.get('/',csrfProtection, function(req, res, next) {
+  res.render('index',
+  { title: 'a/A Express Skeleton Home' ,
+    csrfToken: req.csrfToken()
+});
 });
 
 module.exports = router;
