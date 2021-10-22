c31f104bef214dda8b01a4c48791a567cadb3cbb
Corbin <corbinarcus@hotmail.com>
2021-10-22 16:25:32 -0400

    404 in progress

--- routes/index.js
+++ routes/index.js
@@ -19,22 +19,21 @@ router.get('/',csrfProtection, asyncHandler(async function(req, res, next) {
     as:'answers',
     limit: 1,
     order: [['updatedAt', 'DESC']],
-    include: [{model: db.User}] }],
+    include: [{model: db.User, as: 'user'}] }],
   });
 
   // get comments for a question
-  const comments = await db.Comment.findAll({
-    where : {
-      answerId : 1, //change after answer is configured
-    },
-    order: [['updatedAt', 'DESC']],
-    include: ['user'],
-  })
+  // const comments = await db.Comment.findAll({
+  //   where : {
+  //     answerId : 1, //change after answer is configured
+  //   },
+  //   order: [['updatedAt', 'DESC']],
+  //   include: [{model: db.User, as: 'user'}],
+  // })
 
   res.render( 'index', {
     questions,
     categories,
-    comments,
     csrfToken: req.csrfToken()});
 
 }));
