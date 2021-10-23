51f7ed89347dd9ed96d06de1d6102fde22da336d
suchitra <suchimohan91@gmail.com>
2021-10-22 18:26:31 -0700

    edit question implemented

--- public/javascripts/questionPost.js
+++
@@ -1,6 +0,0 @@
-// const profilePhoto = document.querySelector('.q_profile_photo');
-// profilePhoto.addEventListener('click', async (e) => {
-//     location.href = '/'
-// });
-
-// delete this file? May not be needed.
--- routes/question.js
+++ routes/question.js
@@ -48,7 +48,7 @@ router.get('/:id(\\d+)', csrfProtection, asyncHandler(async (req,res) => {
     //   console.log("////////////////////////////")
     //   console.log(question[0].answers)
 
-    
+
 
     const categories = await db.Category.findAll();
 
@@ -64,9 +64,38 @@ router.get('/:id(\\d+)', csrfProtection, asyncHandler(async (req,res) => {
   }))
 
 
+  router.delete('/:id(\\d+)', async(req, res) => {
+    const questionId = req.params.id
+    const question = await db.Question.findByPk(questionId)
+    if (question) {
+        await question.destroy()
+        res.json({message: "Success"})
+     } else {
+        res.json({message: "Failure"})
+    }
+})
 
 
+ router.post('/:id(\\d+)/edit',csrfProtection, async(req, res) => {
+    const questionId = req.params.id
+    const {
+        title,
+        content,
+        categoryId,
+      } = req.body;
 
+    const question = await db.Question.findByPk(questionId)
+
+    if (question) {
+        await question.update({
+            title: title,
+            content: content,
+            categoryId: categoryId
+        });
+    }
+
+    res.redirect(`/questions/${questionId}`)
+ })
 
 
 module.exports = router;
